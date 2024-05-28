70222bbb3cf85b47141e9a4720565422ea8e37a9
baoliaogeming202001 <167766229+baoliaogeming202001@users.noreply.github.com>
2024-05-28 23:32:17 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -24,25 +24,40 @@ options = [
 for option in options:
     chrome_options.add_argument(option)
 
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+driver_list = []
+driver_numb = 10
+count = 0
+while ( count < driver_numb):
+    driver_list.append( webdriver.Chrome(service=chrome_service, options=chrome_options) )
+    count = count + 1
 
-# 打开传到 tmp.link 的文件，此文件内按行写入下载界面链接
 net_link_file = open("link.txt")
-# 按当前时间新建文件，准备写入 界面链接 文件名 下载链接，此文件会推送到 github 本库根目录
 wsd_link_file = open(time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())+".txt" , "w")
 
-line = net_link_file.readline()
+line = net_link_file.readline().strip('\n')
 
 while line:
-    driver.get(line)
+    count = 0
+    num = 0
+    output_line = []
+    while ( count < driver_numb):
+        if line:
+            output_line.append(line)
+            driver_list[count].get(line)
+            num = num + 1
+        line = net_link_file.readline().strip('\n')
+        count = count + 1
     sleep(10)
-    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[iconpark-icon/@name='rectangle-terminal']/..")))
-    element_content = element.get_attribute("innerHTML")
 
-    lines = element_content.split('\n')
-    wsd_link_file.write(line[:-1]+"\n"+driver.title+"\n"+lines[1][139:-2]+"\n\n")
-    print(line[:-1]+"\n"+driver.title+"\n"+lines[1][139:-2]+"\n\n")
-    line = net_link_file.readline()
+    count1 = 0
+    while ( count1 < num):
+        element = WebDriverWait(driver_list[count1], 10).until(EC.presence_of_element_located((By.XPATH, "//button[iconpark-icon/@name='rectangle-terminal']/..")))
+        element_content = element.get_attribute("innerHTML")
+        lines = element_content.split('\n')
+        output = output_line[count1]+"\n"+driver_list[count1].title+"\n"+lines[1][139:-2]+"\n\n"
+        wsd_link_file.write(output)
+        print(output)
+        count1 = count1 + 1
 
 net_link_file.close()
 wsd_link_file.close()
