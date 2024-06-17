361261ef6df45f5a34ae6901bb0d2e1f74630bb3
baoliaogeming202001 <167766229+baoliaogeming202001@users.noreply.github.com>
2024-06-18 00:44:30 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.chrome.service import Service as ChromeService
+# from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
